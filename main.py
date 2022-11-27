33fcc379fedfc3395c186ca1327cd5475fde23be
VICTOR ROZA SOUZA <enricofermi2019@gmail.com>
2022-11-27 00:48:22 -0400

    main.py com dois get e um put

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from fastapi import FastAPI
+from pydantic import BaseModel
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Produto(BaseModel):
+    id: int
+    nome: str
+    preco: float
+    em_oferta: bool = False
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+produtos = [
+    Produto(id=1, nome='NotebookGamer', preco=4800.00, em_oferta=False),
+    Produto(id=2, nome='Xbox Series S', preco=3800.00, em_oferta=True),
+    Produto(id=3, nome='Box Harry potter', preco=200.00, em_oferta=True),
+    Produto(id=4, nome='Microfone Blue', preco=280.00, em_oferta=True),
+    Produto(id=5, nome='Iphone 14', preco=8280.00, em_oferta=False)
+]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app = FastAPI()
+
+
+@app.get('/')
+async def index():
+    return {"Universo": "Vetorial"}
+
+
+@app.get('/produtos/{id}')
+async def buscar_produto(id: int):
+    for produto in produtos:
+        if produto.id == id:
+            return produto
+    return None
+
+
+@app.put('/produtos/{id}')
+async def atualizar_produto(id: int, produto: Produto):
+    for prod in produtos:
+        if prod.id == id:
+            prod = produto
+
+            return prod
+    return None
